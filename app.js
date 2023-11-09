fe65f98246617747d22fd98943c1186016c92bf3
mileycent <39402443+mileycent@users.noreply.github.com>
2023-11-09 20:15:50 +0100

    linking the css

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose')
 
+
 const app = express();
 
-// const MONGO_URI = process.env.MONGO_URI
-// mongoose.connect(MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true})
-// .then((result) => app.listen(5000, console.log('listening on port 5000')))
-// .catch((err) => console.log(err));
+
 
 app.use(express.urlencoded({ extended: true}))
 app.use(express.static('public'))
@@ -15,6 +13,11 @@ app.set('view engine', 'ejs')
 // routes
 app.use(require('./routes/index'))
 app.use(require('./routes/todo'))
+app.use(express.static('public'))
 
 
-app.listen(5000, () => console.log('Server started at port 5000'))
+// app.listen(5000, () => console.log('Server started at port 5000'))
+const MONGO_URI = 'mongodb+srv://ashton:kutcher@clustercheckpoint3.67g5hl6.mongodb.net/taskmainretryWrites=true&w=majority'
+mongoose.connect(MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true})
+.then((result) => app.listen(5000, console.log('listening on port 5000')))
+.catch((err) => console.log(err));
